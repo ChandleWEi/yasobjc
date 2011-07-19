41a0b99d93f3d22c3d539d5778078e5117d8097f
ChandleWEi <chandle.wei@gmail.com>
2011-07-19 23:24:41 +0800

    add generate.sh

---
+++ generate.sh
@@ -0,0 +1,2 @@
+find /Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS5.0.sdk/System/Library/Frameworks -name "*.h" | xargs yasobjc.rb -o ~/.emacs.d/el-get/yasnippet/snippets/objc-mode
+
